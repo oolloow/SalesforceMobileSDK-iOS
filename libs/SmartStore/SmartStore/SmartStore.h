527c490052bcfc356df23a6eaa071885d8d78a6f
Alexander Zaytsev <az@oolloow.com>
2020-10-12 12:50:23 +1100

    Include non-modular headers override.

--- libs/SmartStore/SmartStore/SmartStore.h
+++ libs/SmartStore/SmartStore/SmartStore.h
@@ -2,9 +2,9 @@
  SmartStore.h
  SmartStore
 
- Created by Alexander Zaytsev on Mon May  6 12:57:00 AEST 2019.
+ Created by Alexander Zaytsev on Mon Oct 12 12:49:44 AEDT 2020.
 
- Copyright (c) 2019-present, salesforce.com, inc. All rights reserved.
+ Copyright (c) 2020-present, salesforce.com, inc. All rights reserved.
  
  Redistribution and use of this software in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
